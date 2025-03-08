9742be0a0e429313fb77a48e89dfd31a9de86f0c
Kent Albores <kentalbores143@gmail.com>
2025-03-08 16:49:24 +0800

    implemented login button

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://sysarch.glitch.me";
 
 const Login = () => {
   const navigate = useNavigate();
-  const handleLogin = () => {
+  const handleLogin = async () => {
     console.log("hell owlord");
-    navigate("/home");
+    try {
+      const response = await axios.post("/api/login", {
+        username: "kenji224",
+        password: "kenken123",
+      });
+      console.log(response.data);
+      navigate("/home");
+    } catch (err) {
+      console.error(err);
+    }
   };
   return (
     <div>
