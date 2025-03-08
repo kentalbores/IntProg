2b7d8b2f53ca23b43433de4d1a5c3cb94008ee67
Kent Albores <kentalbores143@gmail.com>
2025-03-08 17:31:53 +0800

    error trapping for login implementation

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
+import { TextField, Button } from "@mui/material";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://sysarch.glitch.me";
 
 const Login = () => {
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
   const navigate = useNavigate();
   const handleLogin = async () => {
-    console.log("hell owlord");
     try {
       const response = await axios.post("/api/login", {
-        username: "kenji224",
-        password: "kenken123",
+        username: name,
+        password: pass,
       });
-      console.log(response.data);
+      console.log(response);
       navigate("/home");
     } catch (err) {
-      console.error(err);
+      console.log(err.response.data);
+      alert(err.response.data.message);
     }
   };
   return (
     <div>
-      Login page
+      <TextField
+        required
+        id="filled-required"
+        label="Required"
+        defaultValue="username"
+        variant="filled"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        id="filled-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="filled"
+        onChange={(e) => setPass(e.target.value)}
+      />
       <Button onClick={handleLogin} variant="contained" color="success">
         Login
       </Button>
