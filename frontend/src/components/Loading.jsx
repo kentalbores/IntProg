8a19d1e937c0d873fa82b089eeee8ce87ac58581
Kent Albores <kentalbores143@gmail.com>
2025-03-30 04:13:31 +0800

    loading comp

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="50vh"
+      bgcolor="rgba(46, 41, 41, 0.9)"
+      position="absolute"
+      width="30vw"
+    >
+      <CircularProgress size={60} />
+      <Typography variant="h6" mt={2} color="white">
+        Loading...
+      </Typography>
+    </Box>
+    </div>
+  )
+}
+
+export default Loading
